7b8177cedd224d8e4deda9ef616a699213bd0979
Iryn <iryna.tsuper34@gmail.com>
2017-09-08 12:25:31 +0300

    some changes

--- functions.js
+++ functions.js
@@ -1,4 +1,7 @@
-function getRandomColor() {
-  var colors = ['red', 'yellow', 'green', 'blue', 'magenta'];
+const gamma1 = ['red', 'yellow', 'green', 'blue', 'magenta'];
+const gamma2 = ['orange', 'grey', 'purple', 'brown', 'darkblue'];
+
+function getRandomColor(colors) {
+  // var colors = ['red', 'yellow', 'green', 'blue', 'magenta'];
   return colors[Math.floor(Math.random() * colors.length)];
 } // test comment
--- scripts.js
+++ scripts.js
@@ -1,17 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var button, rectangle;
-  button = document.querySelectorAll('.first .btn-primary');
-  // console.log(button.length);
-  button[0].addEventListener("click", function() {
-    rectangle = document.querySelectorAll('.first .inner');
-    // console.log(rectangle.length);
-    rectangle[0].style.backgroundColor = getRandomColor();
+
+  // var gamma1 = ['red', 'yellow', 'green', 'blue', 'magenta'];
+  // var gamma2 = ['orange', 'grey', 'purple', 'brown', 'darkblue'];
+
+  document.querySelectorAll('.first .btn-primary')[0].addEventListener("click", function() {
+    document.querySelectorAll('.first .inner')[0].style.backgroundColor = getRandomColor(gamma1);
   });
 
-  button = document.getElementsByClassName('btn-danger');
-  // console.log(button.length);
-  button[0].addEventListener("click", function() {
-    // console.log(rectangle.length);
-    rectangle[0].style.backgroundColor = 'black';
+  document.querySelectorAll('.third .btn-primary')[0].addEventListener("click",function() {
+    document.querySelectorAll('.third .inner')[0].style.backgroundColor = getRandomColor(gamma2);
+  });
+
+  document.querySelectorAll('.first .btn-danger')[0].addEventListener("click", function() {
+    document.querySelectorAll('.first .inner')[0].style.backgroundColor = 'black';
+  });
+
+  document.querySelectorAll('.third .btn-danger')[0].addEventListener("click", function() {
+    document.querySelectorAll('.third .inner')[0].style.backgroundColor = 'black';
   });
 });
